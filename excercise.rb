e23e74985577caff6067a2cad21e36e5fb466a87
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-10 21:33:08 -0400

    finished excercise2, previous commit was for excercise was 1

--- excercise.rb
+++ excercise.rb
@@ -1,24 +1,35 @@
 fav_colours = ["pink","purple","yellow","orange"]
 age = [29,27,36,28]
 coin_flip = ["tails","tails","tails","heads","heads"]
-artists = ["Beyonce", "Kanye", "Adele"]
+performing_artists = ["Beyonce", "Kanye", "Adele"]
 fav_colours2 = [:pink,:purple,:yellow,:orange]
 
 dictonary = {cunt:"a woman's genitals.",fleek: "To shit on ones education and pull shit out of their ass and call them words.",side_bitch:"A woman that is one level above a jump off but always a step below the wife" }
 
 movie_info = { pulp_fiction:"1994",aladdin:"1992",mission_impossible:"1996"}
 
-places = {London:"8.7 million", Toronto:"2.8 million", Bombay: "20.7 million"}
+population = {London:"8.7 million", Toronto:"2.8 million", Bombay: "20.7 million"}
 
 names = {aman:36,preeti:28,kavita:24,omeet:21}
+#
+# puts coin_flip
+# puts
+# puts fav_colours[0]
+# puts
+# puts "#{age.sort}"
+# puts
+# age << "0"
+# puts age
+# puts
+# puts "#{movie_info[:aladdin]}"
 
-puts coin_flip
-puts
-puts fav_colours[0]
-puts
-puts "#{age.sort}"
-puts
-age << "0"
-puts age
-puts
-puts "#{movie_info[:aladdin]}"
+puts fav_colours[-1]
+
+population[:new_york] = "8.5 million"
+puts population
+
+puts coin_flip.reverse
+
+performing_artists.each do |performing_artists|
+  puts "I think #{performing_artists} is great!"
+end
